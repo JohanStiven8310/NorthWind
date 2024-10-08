ada2fe757fa91d858a05df5ac83ca04d56b83867
JohanStiven8310 <jhanstivn93@gmail.com>
2024-10-08 12:20:09 -0500

    Creacion carpeta ValueObjects y clase UserAction

---
+++ NorthWind.Entities/ValueObjects/UserAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Entities.ValueObjects
+{
+    public class UserAction (string user, string description)
+    {
+        public DateTime CreateDateTime { get; } = DateTime.Now;
+        public string user => user;
+        public string description => description;
+    }
+}
+
