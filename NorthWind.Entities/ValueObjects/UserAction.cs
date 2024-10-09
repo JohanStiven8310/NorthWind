c6ec322161a088fafcc0192a7fbd66aaf29d3b18
JohanStiven8310 <jhanstivn93@gmail.com>
2024-10-09 11:25:40 -0500

    ConsoleWriter

--- NorthWind.Entities/ValueObjects/UserAction.cs
+++ NorthWind.Entities/ValueObjects/UserAction.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace NorthWind.Entities.ValueObjects
 {
-    public class UserAction (string user, string description)
+    public class UserAction (string User, string Description)
     {
         public DateTime CreateDateTime { get; } = DateTime.Now;
-        public string user => user;
-        public string description => description;
+        public string User => User;
+        public string Description => Description;
     }
 }
 
