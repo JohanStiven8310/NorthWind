a86cde61430653f3dbcbc3b965fb0f7e47eea7a4
JohanStiven8310 <jhanstivn93@gmail.com>
2024-10-09 12:47:12 -0500

    Message Program

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -2,7 +2,7 @@
 using NorthWind.Entities.Interfaces;
 using NorthWind.Writers;
 
-IUserActionWriter Writer = new DebugWriter();
+IUserActionWriter Writer = new FileWriter();
 
 Applogger Logger = new Applogger(Writer);
 Logger.WriteLog("Application Started.");
@@ -10,3 +10,11 @@ Logger.WriteLog("Application Started.");
 
 ProductService service = new ProductService(Writer);
 service.Add("Demo", "Azucar refinada");
+
+/*
+ * AppLogger y los Writers: Responsabilidad unica
+ * AppLogger : Abierto pero cerrado
+ * Applogger : Inversio de dependencias, los modulos 
+ * de alto nivel son independientes de los detalles de implementacion
+ * 
+ * */
