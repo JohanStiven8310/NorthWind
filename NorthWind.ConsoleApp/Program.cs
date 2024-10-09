1c1ff6af344ca0cc44986dd34410ab138cc32cdc
JohanStiven8310 <jhanstivn93@gmail.com>
2024-10-09 09:35:34 -0500

    AppLogger - Program

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NorthWind.ConsoleApp.Services;
+using NorthWind.Entities.Interfaces;
+
+IUserActionWriter writer= default;
+
+Applogger Logger = new Applogger(writer);
+Logger.WriteLog("Application Started.");
+
+
--- NorthWind.ConsoleApp/Services/Applogger.cs
+++ NorthWind.ConsoleApp/Services/Applogger.cs
@@ -8,12 +8,13 @@ using System.Threading.Tasks;
 
 namespace NorthWind.ConsoleApp.Services
 {
-    internal class Applogger (IUserActionWriter writer)
+    internal class Applogger (IUserActionWriter writer
     {
         public void WriteLog(string message)
         {
             UserAction Log = new UserAction("System", message);
-            writer.Write(Log);
+          writer.Write(Log);
+
 
         }
     }
